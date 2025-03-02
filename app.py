42afbbdfd61f9362012019394e381ae9f7a83f25
Atharva <147217989+atharvanev@users.noreply.github.com>
2025-03-02 13:50:28 -0800

    initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+# example/st_app.py
+
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import requests 
+
+st.set_page_config(layout="wide")
+
+SPREADSHEET_ID = st.secrets["connections"]["gsheets"]["spreadsheet_id"]
+API_KEY = st.secrets["connections"]["gsheets"]["api_key"]
+
+url = f"https://sheets.googleapis.com/v4/spreadsheets/{SPREADSHEET_ID}?key={API_KEY}"
+response = requests.get(url)
+sheetdata = response.json()
+
+
+worksheet_names = [sheet["properties"]["title"] for sheet in sheetdata.get("sheets", [])]
+
+print(worksheet_names)
+
+conn = st.connection("gsheets", type=GSheetsConnection)
+#data = conn.read(worksheet="Feb2025",usecols = list(range(15)))
+#st.dataframe(data)
+
+
+st.title("UCR Rideshare 🚕")
+col1,col2 = st.columns([2, 4])
+
+with col1:
+    option = st.selectbox('Which Month',worksheet_names)
+    data = conn.read(worksheet=option,usecols = list(range(15)))
+    data["Student's Phone Number"] = data["Student's Phone Number"].astype(str)
+
+with col2:
+    st.dataframe(data)
